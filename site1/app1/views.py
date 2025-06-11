550f5e344ada4cda484efe9f1f28076e79681054
Hnahk14 <khanh.nd.0514@gmail.com>
2025-06-11 15:52:12 +0700

    xém final

--- site1/app1/views.py
+++ site1/app1/views.py
@@ -3,10 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 # from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
     return render(request,'app1/home.html') 
+
 def register (request):
     form = UserCreationForm()
     if request.method == "POST":
@@ -16,6 +21,7 @@ def register (request):
         
     context={'form':form}
     return render(request,'app1/register.html',context)
+
 def loginPage (request):
     # if request.user.is_authenticated:
     #     return redirect('home')
@@ -29,6 +35,26 @@ def loginPage (request):
         else: messages.info(request,'user or password not correct!')
     context={}
     return render(request,'app1/login.html',context)
+
 def logoutPage(request):
     logout(request)
     return redirect('login')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'app1/change_password.html', {
+        'form': form
+    })
+# def testing(request):
+#   template = loader.get_template('home.html')
+#   return HttpResponse(template.render(context, request))
